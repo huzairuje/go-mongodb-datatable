8f40ebc7d9c65fc3a113aaae94a921dd02477c0e
huzairuje <metalh8@gmail.com>
2020-08-05 18:36:38 +0700

    add handler for sample http rest api

--- handler/index.go
+++ handler/index.go
@@ -1,31 +1,59 @@
 package handler
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
 	"path"
 )
 
-type IndexController struct {
-	httpWriter http.ResponseWriter
+type dataSample struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
 
-func (s *IndexController) Index() {
+func Index(writer http.ResponseWriter) {
 	var filepath = path.Join("views", "index.html")
 	var tmpl, err = template.ParseFiles(filepath)
-
 	if err != nil {
-		http.Error(s.httpWriter, err.Error(), http.StatusInternalServerError)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	var data = map[string]interface{}{
-		"title": "Learning Golang Web",
+		"title": "Learning Golang MongoDB",
 		"name":  "Batman",
 	}
-
-	err = tmpl.Execute(s.httpWriter, data)
+	err = tmpl.Execute(writer, data)
 	if err != nil {
-		http.Error(s.httpWriter, err.Error(), http.StatusInternalServerError)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func ListData(writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	var sampleArray []dataSample
+	jsonBytes, err := json.Marshal(sampleArray)
+	if err != nil {
+		writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		writer.WriteHeader(http.StatusInternalServerError)
+		_, _ = writer.Write([]byte(err.Error()))
+	}
+	writer.WriteHeader(http.StatusOK)
+	_, _ = writer.Write(jsonBytes)
+}
+
+func SampleDataJson(writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	sampleData := dataSample{
+		Name:    "John",
+		Address: "Street USA",
+	}
+	jsonBytes, err := json.Marshal(sampleData)
+	if err != nil {
+		writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		writer.WriteHeader(http.StatusInternalServerError)
+		_, _ = writer.Write([]byte(err.Error()))
+	}
+	writer.WriteHeader(http.StatusOK)
+	_, _ = writer.Write(jsonBytes)
 }
--- main.go
+++ main.go
@@ -8,6 +8,17 @@ import (
 func main() {
 	//db := config.CreateConnectionDB()
 	http.HandleFunc("/index", func(writer http.ResponseWriter, request *http.Request) {
-		handler.IndexController.Index(writer)
+		handler.Index(writer)
 	})
+	http.HandleFunc("/sample-data", func(writer http.ResponseWriter, request *http.Request) {
+		handler.SampleDataJson(writer)
+	})
+	http.HandleFunc("/list-data", func(writer http.ResponseWriter, request *http.Request) {
+		handler.ListData(writer)
+	})
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
+	_ = http.ListenAndServe(":9000", nil)
 }
